68aa22da43f237c2f07432f5a11304670e786abe
PedroRocha9 <pedro.lebrerocha@ua.pt>
2022-10-17 18:13:44 +0100

    single and multiple echo effects completed

--- proj1/sndfile-example-src/wav_effects.cpp
+++ proj1/sndfile-example-src/wav_effects.cpp
@@ -10,11 +10,11 @@ constexpr size_t FRAMES_BUFFER_SIZE = 65536; // Buffer for reading frames
 int main(int argc, char *argv[]) {
 
 	if(argc < 3) {
-		cerr << "Usage: " << argv[0] << " <input file> <wanted_effect> [-g gain default (0.5)] [-nr echo_multiplicity default (2)] <output_file>\n";
+		cerr << "Usage: " << argv[0] << " <input file> <output_file> <wanted_effect> <gain> <delay>\n";
 		return 1;
 	}
 
-	SndfileHandle sfhIn { argv[argc-3] };
+	SndfileHandle sfhIn { argv[argc-5] };
 	if(sfhIn.error()) {
 		cerr << "Error: invalid input file\n";
 		return 1;
@@ -30,57 +30,68 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-    SndfileHandle sfhOut { argv[argc-1], SFM_WRITE, sfhIn.format(),
+    SndfileHandle sfhOut { argv[argc-4], SFM_WRITE, sfhIn.format(),
     sfhIn.channels(), sfhIn.samplerate() };
 	if(sfhOut.error()) {
 		cerr << "Error: invalid output file\n";
 		return 1;
     }
 
-    string wanted_effect = argv[argc-2];
-    // check if the wanted_effect is single_echo or multiple_echo
-    if (wanted_effect != "single_echo" || wanted_effect != "multiple_echo") {
-        cerr << "Error: invalid effects\n Usage: wav_effects.cpp <input file> <single_echo [-g gain] [-nr echo_multiplicity] | multiple_echo [-g gain] [-nr echo_multiplicity]> <output_file>\n";
+    string wanted_effect = argv[argc-3];
+    // Check if the wanted_effect is single_echo or multiple_echo
+    float gain;
+    int delay;
+
+    if (wanted_effect != "single_echo" && wanted_effect != "multiple_echo") {
+        cerr << "Error: invalid effects\nUsage: wav_effects.cpp <input file> <output_file> <wanted_effect> <gain> <delay>\n";
         return 1;
     }
-    else if (wanted_effect == "single_echo") {
-        int gain = 0.5;
-        // check if gain is arg, else set gain to 0.5
-        for(int n = 1 ; n < argc ; n++){
-            if(string(argv[n]) == "-g") {
-                gain = stoi(argv[n+1]);
-                break;
-            }
-            // if there's -nr in single echo : wrong arguments
-            else if(string(argv[n]) == "-nr") {
-                cerr << "Error: wrong arguments\n Usage of single echo: wav_effects.cpp <input file> single_echo [-g gain] <output_file>\n";
-                return 1;
-            }
-        }
-    }
-    else if (wanted_effect == "multiple_echo"){
-        // check if gain and/or echo_multiplicity are arg, else set to 0.5, 2      
-        int gain = 0.5;
-        int echo_multiplicity = 2;
-        for(int n = 1 ; n < argc ; n++){
-            if(string(argv[n]) == "-g") {
-                gain = stoi(argv[n+1]);
-            }
-            else if(string(argv[n]) == "-nr"){
-                echo_multiplicity = stoi(argv[n+1]);
-            }
+    else if (wanted_effect == "single_echo" || wanted_effect == "multiple_echo") {
+        // Check if the gain, delay and echo_multiplicity are valid
+        try {
+            gain = stof(argv[argc-2]);
+            delay = stoi(argv[argc-1]);
+        } catch(exception &err) {
+            cerr << "Error: invalid gain or delay\n";
+            return 1;
         }
     }
 
     vector<short> samples(FRAMES_BUFFER_SIZE * sfhIn.channels());
+    vector<short> samples_out;
+    samples_out.resize(0);
     size_t nFrames;
+    short sample_out;
 
-    // create a single echo
-    if (wanted_effect == "single_echo") {
-            sfhOut.writef(&samples[0], nFrames * sfhIn.channels());
+    // Create a single echo
+    if (wanted_effect == "single_echo" || wanted_effect == "multiple_echo") {
+        while((nFrames = sfhIn.readf(samples.data(), FRAMES_BUFFER_SIZE))) {
+            samples.resize(nFrames * sfhIn.channels());
+            
+            for (int i = 0; i < samples.size(); i++) {
+                if (wanted_effect == "single_echo") {
+                    // y(n) = x(n) + a * x(n-delay)
+                    if (i >= delay) {      
+                        sample_out = (samples.at(i) + gain * samples.at(i - delay))/(1 + gain);
+                    } else {
+                        sample_out = samples.at(i);
+                    }
+                } else if (wanted_effect == "multiple_echo") {
+                    // y(n) = x(n) + a * y(n-delay)
+                    if (i >= 1) {
+                        sample_out = (samples.at(i) + gain * samples_out.at(i - 1))/(1 + gain);
+                    } else {
+                        sample_out = samples.at(i);
+                    }
+                }               
+
+                samples_out.insert(samples_out.end(), sample_out);
+            }
+        }
     }
-    // create a multiple echo
-    else if (wanted_effect == "multiple_echo") {
-            sfhOut.writef(&samples[0], nFrames * sfhIn.channels());
-    }
+    
+    // y(n) = x(n) * cos(escolher a freq baixa para se notar mais os valores de oscilação)
+    // cos(2*pi*(f/fa)*n)
+    
+    sfhOut.writef(samples_out.data(), samples_out.size() / sfhIn.channels());
 }
