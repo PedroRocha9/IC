adc020179030da10835da6b30ee1483cd48ea4c0
AndreClerigo Laptop <andreclerigo@outlook.com>
2022-10-31 17:59:51 +0000

    negative working

--- proj2/opencv-src/negative_image.cpp
+++ proj2/opencv-src/negative_image.cpp
@@ -1,15 +1,38 @@
-#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+using namespace std;
+using namespace cv;
 
 int main(int argc, char** argv) {
     //start a timer
     clock_t start = clock();
-
-    cv::Mat invert_image(cv::Mat const& input)
-    {
-        return 255 - input;
+    
+    if (argc < 2) {	
+        cerr << "Usage: " << argv [0] << " <input file>\n" ;
+        return 1 ;
     }
+
+    // Read the image file
+    Mat img = imread(argv[1]);
+
+    for (int i = 0; i < img.rows; i++) {
+        for (int j = 0; j < img.cols * img.channels(); j++) {
+            img.at<uchar>(i, j) = 255 - img.at<uchar>(i, j);
+        }
+    }
+
+    //end the timer
+    clock_t end = clock();
+    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
+    //convert the time to milliseconds
+    elapsed_secs = elapsed_secs * 1000;
+    cout << "Time: " << elapsed_secs << " ms" << endl;
+
+    imshow("Negative Image", img);
+    waitKey(0);
 
     return 0;
 }
