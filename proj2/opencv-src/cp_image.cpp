06a20fc2283299f621b0fdb645bc2e4532c91b33
amaral <jp.amaral@ua.pt>
2022-11-07 16:26:27 +0000

    copy image done

--- proj2/opencv-src/cp_image.cpp
+++ proj2/opencv-src/cp_image.cpp
@@ -25,16 +25,39 @@ int main( int argc, char** argv ) {
         return -1;
     }
 
-    //duplicate the image and save it
-    Mat image2 = image.clone();
-    imwrite(argv[2], image2);
+    //loop through the image and copy the pixels to a new image
+    Mat new_image = Mat::zeros(image.size(), image.type());
+    int channels = image.channels();
+    int nRows = image.rows;
+    int nCols = image.cols * channels;
 
+    //check if the image is continuous
+    if (image.isContinuous()){
+        nCols *= nRows;
+        nRows = 1;
+    }
+
+    //loop through the image and copy the pixels to a new image
+    int i,j;
+    uchar* pixel;
+    for (i = 0; i < nRows; i++){
+        pixel = image.ptr<uchar>(i);
+        //loop through the columns
+        for (j = 0; j < nCols; j++){
+            //copy the pixel to the new image
+            new_image.at<uchar>(i,j) = pixel[j];
+        }
+    }
+
+    //write the new image
+    imwrite(argv[2], new_image);   
 
     //end the timer
     clock_t end = clock();
     double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
     //convert the time to milliseconds
     elapsed_secs = elapsed_secs * 1000;
+    cout << "Time taken: " << elapsed_secs << " ms" << endl;
 
     return 0;
 }
