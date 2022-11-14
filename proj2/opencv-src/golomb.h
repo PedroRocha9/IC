d0be560166b19b7cf97ae9640f6945fd01ece9b1
amaral <jp.amaral@ua.pt>
2022-11-14 17:49:38 +0000

    golomb class working

--- proj2/opencv-src/golomb.h
+++ proj2/opencv-src/golomb.h
@@ -28,16 +28,34 @@ class Golomb {
             for (int i = 0; i < m; i++) {
                 std::string tmp;
                 if (i < n_values_with_min_bits) {
-                    tmp = intToBitString(i, min_bits);
+                    tmp = remaindersBitString(i, min_bits);
                 } 
                 else {
-                    tmp = intToBitString(i + n_values_with_min_bits, max_bits);
+                    tmp = remaindersBitString(i + n_values_with_min_bits, max_bits);
                 }
                 remainders.push_back(tmp);
             }
             return remainders;
 
 
+        }
+
+        // function that converts integer to string of bits with n bits of representation
+        std::string remaindersBitString(int num, int n_bits_representation){
+            std::string result = "";
+            for (int i = 0; i < n_bits_representation; i++) {
+                result = std::to_string(num % 2) + result;
+                num /= 2;
+            }
+            return result;
+        }
+
+        int bitStringToInt(std::string bit_string) {
+            int result = 0;
+            for (int i = 0; i < bit_string.length(); i++) {
+                result = result * 2 + (bit_string[i] - '0');
+            }
+            return result;
         }
         
 
@@ -52,25 +70,92 @@ class Golomb {
                 std::cout << remainders_table[i] << std::endl;
             }
         }
-
+        // implement even-odd implementation and compare results
         // function that converts string of bits to integer
-        int bitStringToInt(std::string bitString) {
+        std::vector<int> bitStringToIntVector(std::string bitString) {
+            std::vector<int> results;
             int result = 0;
+            int countZeros = 0;
+            bool countingZeros = true;
+            int remainder = 0;
+            bool countingRemainder = false;
+            bool readingSignal = false;
+            std::string tmp = "";
             for (int i = 0; i < bitString.length(); i++) {
-                result = result * 2 + (bitString[i] - '0');
+                if (!countingZeros && !readingSignal) {
+                    tmp += bitString[i];
+                    //check if tmp is in remainders_table
+                    for (int j = 0; j < remainders_table.size(); j++) {
+                        if (tmp == remainders_table[j]) {
+                            remainder = j;
+                            tmp = "";
+                            countingRemainder = false;
+                            readingSignal = true;
+                            if (remainder != 0 || countZeros != 0) i++;
+                            
+                            //leave for loop
+                            break;
+                        }
+                    }
+                }
+                if ((bitString[i] == '0') && countingZeros && !readingSignal) {
+                    countZeros++;
+                }
+                if ((bitString[i] == '1') && countingZeros && !countingRemainder && !readingSignal) {
+                    countingZeros = false;
+                    countingRemainder = true;
+                }
+
+                if(readingSignal) {
+                    if (bitString[i] == '1'){
+                        result = ((countZeros * m) + remainder )* (-1);
+                        countingZeros = true;
+
+                    }
+                    else {
+                        result = (countZeros * m) + remainder;
+                        countingZeros = true;
+                    }
+                    results.push_back(result);
+                    result = 0;
+                    countZeros = 0;
+                    remainder = 0;
+                    readingSignal = false;
+                }
+                
+
+                    
+            }
+            return results;
+        }
+
+        std::string intToBitString(int num){
+            //keep the original_number saved for later
+            int original_number = num;
+            if (num < 0){
+                num = -num;
+            }
+            int quotient = num / m;
+            int remainder = num % m;
+            std::string result = "";
+            for (int i = 0; i < quotient; i++) {
+                result += "0";
+            }
+            result += "1";
+            //add the remainder from the table
+            result += remainders_table[remainder];
+            //add the signal for negative numbers after remainder (original_number is different from num)
+            if (num != 0){
+                if (original_number != num) {
+                    result = result + "1";
+                } else {
+                    result = result + "0";
+                }
             }
             return result;
         }
         
-        // function that converts integer to string of bits with n bits of representation
-        std::string intToBitString(int num, int n_bits_representation){
-            std::string result = "";
-            for (int i = 0; i < n_bits_representation; i++) {
-                result = std::to_string(num % 2) + result;
-                num /= 2;
-            }
-            return result;
-        }
+        
         
         // with m bits, we know the number of remainders we can have
         
--- proj2/opencv-src/golomb_encoder.cpp
+++ proj2/opencv-src/golomb_encoder.cpp
@@ -1,6 +1,44 @@
 #include "golomb.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    Golomb g(15);
+    Golomb g(5);
+    string test1 = g.intToBitString(28);
+    string test2 = g.intToBitString(0);
+    string test3 = g.intToBitString(6);
+    string test4 = g.intToBitString(7);
+    string test5 = g.intToBitString(5);
+    string test6 = g.intToBitString(4);
+    string test7 = g.intToBitString(3);
+    string test8 = g.intToBitString(0);
+    string test9 = g.intToBitString(2);
+    string test10 = g.intToBitString(10);
+    string test11 = g.intToBitString(11);
+    string test12 = g.intToBitString(1);
+    string test13 = g.intToBitString(13);
+    string test14 = g.intToBitString(14);
+    string test15 = g.intToBitString(-16);
+    string test16 = g.intToBitString(4);
+    string test17 = g.intToBitString(-5);
+    string test18 = g.intToBitString(-7);
+    string test19 = g.intToBitString(1);
+    string test20 = g.intToBitString(-70);
+    string test21 = g.intToBitString(0);
+    string test22 = g.intToBitString(-98);
+
+    string total = test1 + test2 + test3 + test4 + test5 + test6 + test7 + test8 + test9 + test10 + test11 + test12 + test13 + test14 + test15 + test16 + test17 + test18 + test19 + test20 + test21 + test22;
+    cout << total << endl;
+    cout << "num bits: " << total.length() << endl;
+    vector<int> decoded = g.bitStringToIntVector(total);
+    cout << "---------------------" << endl;
+    for (int i = 0; i < decoded.size(); i++) {
+        cout << decoded[i] << endl;
+    }
+
+
     return 0;
 }
