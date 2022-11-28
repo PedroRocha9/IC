321373d9ad31909ffdbbc40f0c28712775157706
PedroRocha9 <pedro.lebrerocha@ua.pt>
2022-11-28 23:47:00 +0000

    solving merge conflicts, CMakeList updated and img_encoder progress

--- proj2/opencv-src/image_encoder.cpp
+++ proj2/opencv-src/image_encoder.cpp
@@ -9,24 +9,7 @@
 #include "bitStream.h"
 
 using namespace std;
-
-// function to get the rgb values of the image's matrix and converts them to YCvCr
-void rgb2ycbcr(cv::Mat &image) {
-    for (int i = 0; i < image.rows; i++) {
-        for (int j = 0; j < image.cols; j++) {
-            cv::Vec3b &pixel = image.at<cv::Vec3b>(i, j);
-            int r = pixel[2];
-            int g = pixel[1];
-            int b = pixel[0];
-            int y = (int) (0.299 * r + 0.587 * g + 0.114 * b);
-            int cb = (int) (128 - 0.168736 * r - 0.331264 * g + 0.5 * b);
-            int cr = (int) (128 + 0.5 * r - 0.418688 * g - 0.081312 * b);
-            pixel[0] = y;
-            pixel[1] = cb;
-            pixel[2] = cr;
-        }
-    }
-}
+using namespace cv;
 
 // function that calculates the average of 3 numbers
 int average(int a, int b, int c) {
@@ -36,17 +19,88 @@ int average(int a, int b, int c) {
 //function that calculates the prediction of the next value in the sequence based on 3 values -> pixels to the left, above and to the left top
 // if the matrix it's in the first row of the matrix, use only the previous pixel (to the left)
 // if it's the first pixel of the matrix, do not use prediction obviously
-int predict(){
-    return 0;
-}
  
 
-// jpeg type of prediction using YCvCr to YCvCr (opencv converts rgb to YCvCr) 
+// jpeg type of prediction using YCbCr to YCbCr (opencv converts rgb to YCbCr) 
 // when having only 1 line of pixels, predict based on the previous pixel (to the left), like a jpeg encoder
 // there's always 3 channels/spaces in the image --> não entendi bem isto xD é Y, Cb e Cr?
 // Predict the pixel value based on the pixel value above it, to the left of it and to the left top of it
 
 int main(int argc, char *argv[]){
+
+     //start a timer
+    clock_t start = clock();
+
+    // check if the number of arguments is correct
+    if(argc < 3) {
+        cerr << "Usage: " << argv[0] << " <input file> <output file>\n";
+        return 1;
+    }
+
+    // read the image
+    Mat img = imread(argv[1]);
+    // check if the image is loaded
+    if (img.empty()){
+        cout << "Could not load image: " << argv[1] << endl;
+        return -1;
+    }
+
+    // get the rgb values of the image's matrix and converts them to YCbCr
+    for (int i = 0; i < img.rows; i++) {
+        for (int j = 0; j < img.cols; j++) {
+            cv::Vec3b &pixel = img.at<cv::Vec3b>(i, j);
+
+            int y = (int) (0.299 * pixel[2] + 0.587 * pixel[1] + 0.114 * pixel[0]);
+            int cb = (int) (128 - 0.168736 * pixel[2] - 0.331264 * pixel[1] + 0.5 * pixel[0]);
+            int cr = (int) (128 + 0.5 * pixel[2] - 0.418688 * pixel[1] - 0.081312 * pixel[0]);
+
+            pixel[0] = y;
+            pixel[1] = cb;
+            pixel[2] = cr;
+            
+            // print the first row of pixels with the YCbCr values
+            cout << "Y: " << y << " Cb: " << cb << " Cr: " << cr << endl;
+        }
+    }
+
+    //function that calculates average of 3 numbers
+    auto average = [](int a, int b, int c) {
+        return (a + b + c) / 3;
+    };
+
+    //function that predicts the next value in the sequence based on 3 previous values
+    auto predict = [](int a, int b, int c) {
+        //3*a - 3*b + c
+        return 3*a - 3*b + c;
+    };
+
+    vector<int> m_vector;
+    vector<int> valuesToBeEncoded;
+    
+    //print the first pixel of the img
+    cout << "First pixel: " << img.at<cv::Vec3b>(0, 0) << endl;
+
+    if (img.rows == 1){   
+        for(int i = 0; i < img.rows; i++) {
+            for(int j=0; j < img.cols; j++){
+                //int prediction = predict(img[i,j], 0, 0);
+                // int m = m - prediction;
+                // m_vector.push_back(m);
+                // valuesToBeEncoded.push_back(m);
+            }
+        }
+    }
+    else {
+    }
+    
+    //end the timer
+    clock_t end = clock();
+
+    double elapsed_secs = double(end - start) / CLOCKS_PER_SEC;
+    //convert the time to milliseconds
+    elapsed_secs = elapsed_secs * 1000;
+    cout << "Execution time: " << elapsed_secs << " ms" << endl;
+ 
     return 0;
 }
 
