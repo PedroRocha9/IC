deb028ab17ac31bb0a7e687db1954f42d28aa8d1
PedroRocha9 <pedro.lebrerocha@ua.pt>
2022-12-05 17:16:38 +0000

    irrelevant

--- proj3/video-src/hello.cpp
+++ proj3/video-src/hello.cpp
@@ -3,6 +3,9 @@ using namespace std;
 int main(int argc, int **argv){
     // print hello world
     // opencv transforms _cif.y4m in FRAMES, because the file has the structure:
-    // YUV4MPEG2 W352 H288 
+    // YUV4MPEG2 W352 H288 Frame rate 30000:1001 (~30 frames per second) 
+    // FRAMES
+    // ... binary info
+    // FRAMES
     return 0;
 }
