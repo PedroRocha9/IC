a2262fbf4d3c366b5cf8071ebc643685ec247f36
PedroRocha9 <pedro.lebrerocha@ua.pt>
2022-12-05 17:15:36 +0000

    setting up repo pt.2

---
+++ proj3/video-src/hello.cpp
@@ -0,0 +1,8 @@
+using namespace std;
+
+int main(int argc, int **argv){
+    // print hello world
+    // opencv transforms _cif.y4m in FRAMES, because the file has the structure:
+    // YUV4MPEG2 W352 H288 
+    return 0;
+}
